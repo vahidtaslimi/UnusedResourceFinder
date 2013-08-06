308f0fe4b4dc4586736dce262d9635e121cae2cd
vahidtaslimi <vahid.taslimi@gmail.com>
2013-08-06 10:46:45 +1000

    initial add

---
+++ UnusedResourceFinder/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace UnusedResourceFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtResourceFile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFileTypes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.filesProgressBar = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.rdbWin = new System.Windows.Forms.RadioButton();
+            this.rdbiOS = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Files location";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(9, 91);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(544, 20);
+            this.txtFolder.TabIndex = 1;
+            this.txtFolder.Text = "C:\\TFS\\Mobile\\Liberado\\Current\\iOS\\Liberado\\Liberado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbiOS);
+            this.groupBox1.Controls.Add(this.rdbWin);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtResourceFile);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtFileTypes);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(932, 214);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(559, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(744, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtResourceFile
+            // 
+            this.txtResourceFile.Location = new System.Drawing.Point(9, 41);
+            this.txtResourceFile.Name = "txtResourceFile";
+            this.txtResourceFile.Size = new System.Drawing.Size(544, 20);
+            this.txtResourceFile.TabIndex = 6;
+            this.txtResourceFile.Text = "C:\\TFS\\Mobile\\Liberado\\Current\\iOS\\Liberado\\Liberado\\SupportingFiles\\en.lproj\\Loc" +
+    "alizable.strings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resource file";
+            // 
+            // txtFileTypes
+            // 
+            this.txtFileTypes.Location = new System.Drawing.Point(9, 148);
+            this.txtFileTypes.Name = "txtFileTypes";
+            this.txtFileTypes.Size = new System.Drawing.Size(544, 20);
+            this.txtFileTypes.TabIndex = 4;
+            this.txtFileTypes.Text = "m,h";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File extensions to exclude(comma seperated)";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.filesProgressBar);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtLog);
+            this.groupBox2.Controls.Add(this.txtResult);
+            this.groupBox2.Location = new System.Drawing.Point(12, 243);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(932, 550);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // filesProgressBar
+            // 
+            this.filesProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filesProgressBar.Location = new System.Drawing.Point(9, 521);
+            this.filesProgressBar.Name = "filesProgressBar";
+            this.filesProgressBar.Size = new System.Drawing.Size(917, 23);
+            this.filesProgressBar.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(9, 489);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(917, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(559, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Unused resource strings";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Log";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(9, 40);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(544, 443);
+            this.txtLog.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(568, 40);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(358, 443);
+            this.txtResult.TabIndex = 0;
+            // 
+            // rdbWin
+            // 
+            this.rdbWin.AutoSize = true;
+            this.rdbWin.Checked = true;
+            this.rdbWin.Location = new System.Drawing.Point(12, 187);
+            this.rdbWin.Name = "rdbWin";
+            this.rdbWin.Size = new System.Drawing.Size(65, 17);
+            this.rdbWin.TabIndex = 9;
+            this.rdbWin.TabStop = true;
+            this.rdbWin.Text = "Resx file";
+            this.rdbWin.UseVisualStyleBackColor = true;
+            // 
+            // rdbiOS
+            // 
+            this.rdbiOS.AutoSize = true;
+            this.rdbiOS.Location = new System.Drawing.Point(122, 186);
+            this.rdbiOS.Name = "rdbiOS";
+            this.rdbiOS.Size = new System.Drawing.Size(102, 17);
+            this.rdbiOS.TabIndex = 10;
+            this.rdbiOS.Text = "iOS resource file";
+            this.rdbiOS.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 805);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtResourceFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFileTypes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar filesProgressBar;
+        private System.Windows.Forms.RadioButton rdbiOS;
+        private System.Windows.Forms.RadioButton rdbWin;
+    }
+}
+
---
+++ UnusedResourceFinder/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace UnusedResourceFinder
+{
+    using System.Collections;
+    using System.IO;
+    using System.Resources;
+
+    public partial class Form1 : Form
+    {
+        #region Fields
+
+        private List<string> fileTypes = null;
+
+        private List<string> files = null;
+
+        private List<string> resources = null;
+
+        private List<string> unusedResources;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static string GetFileText(string name)
+        {
+            string fileContents = string.Empty;
+
+            if (System.IO.File.Exists(name))
+            {
+                fileContents = System.IO.File.ReadAllText(name);
+            }
+
+            return fileContents;
+        }
+
+        private void FindUnusedResources()
+        {
+            var content = string.Empty;
+            string filename = string.Empty;
+            try
+            {
+                foreach (var s in this.resources)
+                {
+                    this.progressBar1.Value += 1;
+                    this.filesProgressBar.Value = 0;
+                    this.Log(string.Format("{1}****************** {0}", s, Environment.NewLine));
+                    bool foundResource = false;
+                    if (string.IsNullOrWhiteSpace(s))
+                    {
+                        continue;
+                    }
+
+                    foreach (var file in this.files)
+                    {
+                        this.filesProgressBar.Value += 1;
+                        filename = file;
+                        content = File.ReadAllText(file);
+                        if (string.IsNullOrWhiteSpace(content))
+                        {
+                            continue;
+                        }
+
+                        if (content.Contains(s))
+                        {
+                            this.Log(string.Format("Is used in {0}", file));
+                            foundResource = true;
+                        }
+                    }
+
+                    if (foundResource == false && this.unusedResources.Contains(s) == false)
+                    {
+                        this.Log(string.Format("{0} is UNUSED", s));
+                        this.RecordResult(s);
+                        this.unusedResources.Add(s);
+                    }
+                }
+
+                this.filesProgressBar.Value = this.filesProgressBar.Maximum;
+                this.progressBar1.Value = this.progressBar1.Maximum;
+            }
+            catch (Exception ex)
+            {
+                this.Log(string.Format("Could not read file {0}. This exception occured: {1}", filename, ex.Message));
+            }
+        }
+
+        private void GetAllFilesInFolder(string folderToSearch)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(folderToSearch))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        if (this.IsValidFileType(f))
+                        {
+                            this.files.Add(f);
+                        }
+                    }
+
+                    this.GetAllFilesInFolder(d);
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private List<string> GetFileTypes()
+        {
+            var result = new List<string>();
+            if (string.IsNullOrWhiteSpace(this.txtFileTypes.Text))
+            {
+                return result;
+            }
+
+            foreach (var s in this.txtFileTypes.Text.Split(','))
+            {
+                if (!string.IsNullOrWhiteSpace(s))
+                {
+                    result.Add(s.Trim().ToLower());
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsValidFileType(string file)
+        {
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                return false;
+            }
+
+            file = file.ToLower();
+            foreach (var fileType in this.fileTypes)
+            {
+                if (file.EndsWith(fileType))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void LoadFiles()
+        {
+            string startFolder = this.txtFolder.Text;
+            var dir = new System.IO.DirectoryInfo(startFolder);
+            IEnumerable<FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            string resourceFilename = this.txtResourceFile.Text;
+            resourceFilename = resourceFilename.Substring(0, resourceFilename.LastIndexOf('.'));
+
+            var queryMatchingFiles = from file in fileList
+                                     where
+                                         this.IsValidFileType(file.FullName)
+                                         && file.FullName.StartsWith(resourceFilename) == false
+                                     let fileText = GetFileText(file.FullName)
+                                     // where fileText.Contains(searchTerm)
+                                     select file.FullName;
+            this.files.AddRange(queryMatchingFiles);
+        }
+
+        private void LoadIosResources()
+        {
+            this.resources = new List<string>();
+            var lines = File.ReadAllLines(this.txtResourceFile.Text);
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("\"") == false)
+                {
+                    continue;
+                }
+
+                var key = line.Split('=')[0];
+                key = key.Trim();
+                key = key.TrimStart(new[] { '"' });
+                key = key.TrimEnd(new[] { '"' });
+                this.resources.Add(key);
+            }
+        }
+
+        private void LoadWindowsResources()
+        {
+            this.resources = new List<string>();
+            var resourceReader = new ResXResourceReader(this.txtResourceFile.Text);
+            bool isReswFile = this.txtResourceFile.Text.EndsWith(".resw");
+            string key = string.Empty;
+            foreach (DictionaryEntry r in resourceReader)
+            {
+                if (isReswFile)
+                {
+                    key = r.Key.ToString();
+                    key = key.Substring(0, key.LastIndexOf('.'));
+                    this.resources.Add(key);
+                }
+                else
+                {
+                    this.resources.Add(r.Key.ToString());
+                }
+            }
+        }
+
+        private void Log(string message)
+        {
+            this.txtLog.AppendText(message + Environment.NewLine);
+            Application.DoEvents();
+        }
+
+        private void RecordResult(string message)
+        {
+            this.txtResult.AppendText(message + Environment.NewLine);
+            Application.DoEvents();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFolder.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.progressBar1.Minimum = 0;
+            this.progressBar1.Value = 0;
+            this.filesProgressBar.Minimum = 0;
+            this.filesProgressBar.Value = 0;
+            this.txtResult.Clear();
+            this.fileTypes = this.GetFileTypes();
+            this.files = new List<string>();
+            this.unusedResources = new List<string>();
+            this.LoadFiles();
+            this.Log(string.Format("Found {0} to search", this.files.Count));
+            if (this.rdbiOS.Checked)
+            {
+                this.LoadIosResources();
+            }
+            else
+            {
+                this.LoadWindowsResources();
+            }
+
+            this.progressBar1.Maximum = this.resources.Count;
+            this.filesProgressBar.Maximum = this.files.Count;
+            Application.DoEvents();
+            this.FindUnusedResources();
+
+            this.txtResult.AppendText(Environment.NewLine + "Finished Searching");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtResourceFile.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        #endregion
+    }
+}
